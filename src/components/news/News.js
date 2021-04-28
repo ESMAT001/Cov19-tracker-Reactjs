20027db9e92db6b747a29e2eed79b3e25b86f0ac
ESMAT001 <esmat10.niazi@gmail.com>
2021-04-28 18:29:16 +0430

    modal added

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { countryNewsLink, worldNewsLink } from '../../util';
-
+import Modal from './Modal';
 
 
 function News({ countryName, countryCode }) {
@@ -30,24 +30,32 @@ function News({ countryName, countryCode }) {
     }, [])
 
     const renderNews = (news, index) => {
-        let { nid, urlToImage, url, title, publishedAt, siteName, description, content } = news;
+        let { nid, urlToImage, title, description } = news;
         return (
-            <div key={nid + "-" + index} className="flex flex-row">
-                <div>
-                    <img src={urlToImage} alt={title} />
+            <div key={nid + "-" + index} className="bg-gray-200 bg-opacity-10 dark:bg-gray-500 dark:bg-opacity-5 flex flex-col md:flex-row rounded shadow-lg overflow-hidden items-center">
+                <div className="md:w-2/6">
+                    <img src={urlToImage} className="object-cover " alt={title} />
                 </div>
-                <div>
-                    <h3>{title}</h3>
-                    <p>{description}</p>
+                <div className="p-6 md:w-4/6">
+                    <h3 className='text-xl font-semibold opacity-90'>{title}</h3>
+                    <hr className=" md:hidden my-2 bg-gray-500 bg-opacity-70 shadow rounded" />
+                    <p className="opacity-80 font-semibold  mt-2">{description}</p>
+                    {/* <button className="bg-blue-600 text-white px-3 py-2 font-semibold shadow-lg rounded focus:outline-none hover:shadow-xl hover:bg-blue-700  transition duration-300 float-right mt-6">Read More</button> */}
+                    <Modal newsInfo={news} />
                 </div>
             </div>
         )
     }
+
+
+    const cls = "flex flex-col space-y-10";
+
+    const newsHeadingCls = "text-xl opacity-90 py-4 font-semibold text-center border-b-2 border-gray-400 border-opacity-80";
     return (
-        <div className="text-indigo-50">
+        <div className="dark:text-indigo-50 text-gray-600">
             {
-                countryNewsData && <div className="flex flex-col">
-                    <h4>
+                countryNewsData && <div className={cls}>
+                    <h4 className={newsHeadingCls}>
                         Covid-19 news in {countryName}
                     </h4>
                     {
@@ -58,10 +66,12 @@ function News({ countryName, countryCode }) {
                 </div>
             }
             {
-                worldNewsData && <div>
-                    <h4>
-                        World news about covid19
-                </h4>
+                worldNewsData && <div className={cls}>
+                    <p className="border-gray-500 border-opacity-80 text-opacity-90" ></p>
+                    <h4 className={newsHeadingCls}>
+                        World news about covid-19
+                     </h4>
+                    <img src="" className="" alt="" />
                     {
                         worldNewsData.map((news, index) => {
                             return renderNews(news, index);

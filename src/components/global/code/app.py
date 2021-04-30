c19a5b0c642d91668b5706ecb33f901b7f9b23db
ESMAT001 <esmat10.niazi@gmail.com>
2021-04-30 10:38:48 +0430

    an commit

--- src/components/global/code/app.py
+++ src/components/global/code/app.py
@@ -9,7 +9,7 @@ for _ in range(int(sys.argv[1])):
         for _ in range(rand):
             f.write(f"elif x1=={i} and x2=={i+1}:\n\tprint(x1+x2)\n")
             i+=1
-        f.write('""" --IM_ES_N """\n')
+        f.write('#--IM_ES_N --\n')
     system('git add .')
     system('git commit -m "an commit"')
     sleep(randint(0,3))
--- src/components/global/code/temp.py
+++ src/components/global/code/temp.py
@@ -1,3 +1,9 @@
+x1=int(input("x1"))
+x2=int(input("x2"))
+
+
+if x1==x2:
+    print(x1+x2)
 elif x1==0 and x2==1:
 	print(x1+x2)
 elif x1==1 and x2==2:
@@ -8,39 +14,4 @@ elif x1==3 and x2==4:
 	print(x1+x2)
 elif x1==4 and x2==5:
 	print(x1+x2)
-""" --IM_ES_N """
-elif x1==5 and x2==6:
-	print(x1+x2)
-elif x1==6 and x2==7:
-	print(x1+x2)
-elif x1==7 and x2==8:
-	print(x1+x2)
-elif x1==8 and x2==9:
-	print(x1+x2)
-elif x1==9 and x2==10:
-	print(x1+x2)
-elif x1==10 and x2==11:
-	print(x1+x2)
-""" --IM_ES_N """
-elif x1==11 and x2==12:
-	print(x1+x2)
-elif x1==12 and x2==13:
-	print(x1+x2)
-elif x1==13 and x2==14:
-	print(x1+x2)
-elif x1==14 and x2==15:
-	print(x1+x2)
-elif x1==15 and x2==16:
-	print(x1+x2)
-""" --IM_ES_N """
-elif x1==16 and x2==17:
-	print(x1+x2)
-elif x1==17 and x2==18:
-	print(x1+x2)
-elif x1==18 and x2==19:
-	print(x1+x2)
-elif x1==19 and x2==20:
-	print(x1+x2)
-elif x1==20 and x2==21:
-	print(x1+x2)
-""" --IM_ES_N """
+#--IM_ES_N --

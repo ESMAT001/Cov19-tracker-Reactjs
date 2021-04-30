856813f41b5f4f0079914336e52de4ed25730124
ESMAT001 <esmat10.niazi@gmail.com>
2021-04-30 10:39:59 +0430

    an commit

--- src/components/global/code/temp.py
+++ src/components/global/code/temp.py
@@ -4,6 +4,7 @@ x2=int(input("x2"))
 
 if x1==x2:
     print(x1+x2)
+
 elif x1==0 and x2==1:
 	print(x1+x2)
 elif x1==1 and x2==2:
@@ -14,7 +15,6 @@ elif x1==3 and x2==4:
 	print(x1+x2)
 elif x1==4 and x2==5:
 	print(x1+x2)
-#--IM_ES_N --
 elif x1==5 and x2==6:
 	print(x1+x2)
 elif x1==6 and x2==7:
@@ -27,7 +27,6 @@ elif x1==9 and x2==10:
 	print(x1+x2)
 elif x1==10 and x2==11:
 	print(x1+x2)
-#--IM_ES_N --
 elif x1==11 and x2==12:
 	print(x1+x2)
 elif x1==12 and x2==13:
@@ -40,7 +39,6 @@ elif x1==15 and x2==16:
 	print(x1+x2)
 elif x1==16 and x2==17:
 	print(x1+x2)
-#--IM_ES_N --
 elif x1==17 and x2==18:
 	print(x1+x2)
 elif x1==18 and x2==19:
@@ -50,5 +48,145 @@ elif x1==19 and x2==20:
 elif x1==20 and x2==21:
 	print(x1+x2)
 elif x1==21 and x2==22:
+	print(x1+x2)
+elif x1==22 and x2==23:
+	print(x1+x2)
+elif x1==23 and x2==24:
+	print(x1+x2)
+elif x1==24 and x2==25:
+	print(x1+x2)
+elif x1==25 and x2==26:
+	print(x1+x2)
+elif x1==26 and x2==27:
+	print(x1+x2)
+elif x1==27 and x2==28:
+	print(x1+x2)
+elif x1==28 and x2==29:
+	print(x1+x2)
+elif x1==29 and x2==30:
+	print(x1+x2)
+elif x1==30 and x2==31:
+	print(x1+x2)
+elif x1==31 and x2==32:
+	print(x1+x2)
+elif x1==32 and x2==33:
+	print(x1+x2)
+elif x1==33 and x2==34:
+	print(x1+x2)
+elif x1==34 and x2==35:
+	print(x1+x2)
+elif x1==35 and x2==36:
+	print(x1+x2)
+elif x1==36 and x2==37:
+	print(x1+x2)
+elif x1==37 and x2==38:
+	print(x1+x2)
+elif x1==38 and x2==39:
+	print(x1+x2)
+elif x1==39 and x2==40:
+	print(x1+x2)
+elif x1==40 and x2==41:
+	print(x1+x2)
+elif x1==41 and x2==42:
+	print(x1+x2)
+elif x1==42 and x2==43:
+	print(x1+x2)
+elif x1==43 and x2==44:
+	print(x1+x2)
+elif x1==44 and x2==45:
+	print(x1+x2)
+elif x1==45 and x2==46:
+	print(x1+x2)
+elif x1==46 and x2==47:
+	print(x1+x2)
+elif x1==47 and x2==48:
+	print(x1+x2)
+elif x1==48 and x2==49:
+	print(x1+x2)
+elif x1==49 and x2==50:
+	print(x1+x2)
+elif x1==50 and x2==51:
+	print(x1+x2)
+elif x1==51 and x2==52:
+	print(x1+x2)
+elif x1==52 and x2==53:
+	print(x1+x2)
+elif x1==53 and x2==54:
+	print(x1+x2)
+elif x1==54 and x2==55:
+	print(x1+x2)
+elif x1==55 and x2==56:
+	print(x1+x2)
+elif x1==56 and x2==57:
+	print(x1+x2)
+elif x1==57 and x2==58:
+	print(x1+x2)
+elif x1==58 and x2==59:
+	print(x1+x2)
+elif x1==59 and x2==60:
+	print(x1+x2)
+elif x1==60 and x2==61:
+	print(x1+x2)
+elif x1==61 and x2==62:
+	print(x1+x2)
+elif x1==62 and x2==63:
+	print(x1+x2)
+elif x1==63 and x2==64:
+	print(x1+x2)
+elif x1==64 and x2==65:
+	print(x1+x2)
+elif x1==65 and x2==66:
+	print(x1+x2)
+elif x1==66 and x2==67:
+	print(x1+x2)
+elif x1==67 and x2==68:
+	print(x1+x2)
+elif x1==68 and x2==69:
+	print(x1+x2)
+elif x1==69 and x2==70:
+	print(x1+x2)
+elif x1==70 and x2==71:
+	print(x1+x2)
+elif x1==71 and x2==72:
+	print(x1+x2)
+elif x1==72 and x2==73:
+	print(x1+x2)
+elif x1==73 and x2==74:
+	print(x1+x2)
+elif x1==74 and x2==75:
+	print(x1+x2)
+elif x1==75 and x2==76:
+	print(x1+x2)
+elif x1==76 and x2==77:
+	print(x1+x2)
+elif x1==77 and x2==78:
+	print(x1+x2)
+elif x1==78 and x2==79:
+	print(x1+x2)
+elif x1==79 and x2==80:
+	print(x1+x2)
+elif x1==80 and x2==81:
+	print(x1+x2)
+elif x1==81 and x2==82:
+	print(x1+x2)
+elif x1==82 and x2==83:
+	print(x1+x2)
+elif x1==83 and x2==84:
+	print(x1+x2)
+elif x1==84 and x2==85:
+	print(x1+x2)
+elif x1==85 and x2==86:
+	print(x1+x2)
+elif x1==86 and x2==87:
+	print(x1+x2)
+elif x1==87 and x2==88:
+	print(x1+x2)
+elif x1==88 and x2==89:
+	print(x1+x2)
+elif x1==89 and x2==90:
+	print(x1+x2)
+elif x1==90 and x2==91:
+	print(x1+x2)
+elif x1==91 and x2==92:
 	print(x1+x2)
 #--IM_ES_N --

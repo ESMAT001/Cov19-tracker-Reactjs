e5a3862542722030851e3907ce969c1077cc8043
ESMAT001 <esmat10.niazi@gmail.com>
2021-04-30 10:34:40 +0430

    an commit

--- src/components/global/code/app.py
+++ src/components/global/code/app.py
@@ -2,11 +2,12 @@ from os import system
 import sys
 from time import sleep
 from random import randint
+i=0
 for _ in range(int(sys.argv[1])):
     rand=randint(int(sys.argv[2]),int(sys.argv[3]))
     with open('./temp.py','a') as f:
         for _ in range(rand):
-            f.write("print('Hello world')\n")
+            f.write(f"elif x1=={i} and x2=={i+1}:\n\tprint(x1+x2)")
         f.write('""" --IM_ES_N """\n')
     system('git add .')
     system('git commit -m "an commit"')
--- src/components/global/code/temp.py
+++ src/components/global/code/temp.py
@@ -25,3 +25,10 @@ print('Hello world')
 print('Hello world')
 print('Hello world')
 """ --IM_ES_N """
+elif x1==0 and x2==1:
+	print(x1+x2)elif x1==0 and x2==1:
+	print(x1+x2)elif x1==0 and x2==1:
+	print(x1+x2)elif x1==0 and x2==1:
+	print(x1+x2)elif x1==0 and x2==1:
+	print(x1+x2)elif x1==0 and x2==1:
+	print(x1+x2)""" --IM_ES_N """

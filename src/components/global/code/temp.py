5ac967e8530ed89d6892d96ede3112c2369b7c22
ESMAT001 <esmat10.niazi@gmail.com>
2021-04-30 10:31:29 +0430

    an commit

--- src/components/global/code/temp.py
+++ src/components/global/code/temp.py
@@ -4,3 +4,10 @@ print('Hello world')
 print('Hello world')
 print('Hello world')
 """ --IM_ES_N """
+print('Hello world')
+print('Hello world')
+print('Hello world')
+print('Hello world')
+print('Hello world')
+print('Hello world')
+""" --IM_ES_N """

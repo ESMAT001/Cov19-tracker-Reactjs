bf813371ff880b26774c93e330466963c4453630
ESMAT001 <esmat10.niazi@gmail.com>
2021-04-30 10:38:52 +0430

    an commit

--- src/components/global/code/temp.py
+++ src/components/global/code/temp.py
@@ -15,3 +15,16 @@ elif x1==3 and x2==4:
 elif x1==4 and x2==5:
 	print(x1+x2)
 #--IM_ES_N --
+elif x1==5 and x2==6:
+	print(x1+x2)
+elif x1==6 and x2==7:
+	print(x1+x2)
+elif x1==7 and x2==8:
+	print(x1+x2)
+elif x1==8 and x2==9:
+	print(x1+x2)
+elif x1==9 and x2==10:
+	print(x1+x2)
+elif x1==10 and x2==11:
+	print(x1+x2)
+#--IM_ES_N --

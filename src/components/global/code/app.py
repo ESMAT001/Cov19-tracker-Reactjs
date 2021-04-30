1c57e1684adec47907fa0f503aea6c209e0c5bd2
ESMAT001 <esmat10.niazi@gmail.com>
2021-04-30 10:36:02 +0430

    an commit

--- src/components/global/code/app.py
+++ src/components/global/code/app.py
@@ -7,7 +7,8 @@ for _ in range(int(sys.argv[1])):
     rand=randint(int(sys.argv[2]),int(sys.argv[3]))
     with open('./temp.py','a') as f:
         for _ in range(rand):
-            f.write(f"elif x1=={i} and x2=={i+1}:\n\tprint(x1+x2)")
+            f.write(f"elif x1=={i} and x2=={i+1}:\n\tprint(x1+x2)\n")
+            i+=1
         f.write('""" --IM_ES_N """\n')
     system('git add .')
     system('git commit -m "an commit"')
--- src/components/global/code/temp.py
+++ src/components/global/code/temp.py
@@ -1,54 +1,11 @@
-print('Hello world')
-print('Hello world')
-print('Hello world')
-print('Hello world')
-print('Hello world')
-""" --IM_ES_N """
-print('Hello world')
-print('Hello world')
-print('Hello world')
-print('Hello world')
-print('Hello world')
-print('Hello world')
-""" --IM_ES_N """
-print('Hello world')
-print('Hello world')
-print('Hello world')
-print('Hello world')
-print('Hello world')
-print('Hello world')
-""" --IM_ES_N """
-print('Hello world')
-print('Hello world')
-print('Hello world')
-print('Hello world')
-print('Hello world')
-print('Hello world')
-""" --IM_ES_N """
 elif x1==0 and x2==1:
-	print(x1+x2)elif x1==0 and x2==1:
-	print(x1+x2)elif x1==0 and x2==1:
-	print(x1+x2)elif x1==0 and x2==1:
-	print(x1+x2)elif x1==0 and x2==1:
-	print(x1+x2)elif x1==0 and x2==1:
-	print(x1+x2)""" --IM_ES_N """
-elif x1==0 and x2==1:
-	print(x1+x2)elif x1==0 and x2==1:
-	print(x1+x2)elif x1==0 and x2==1:
-	print(x1+x2)elif x1==0 and x2==1:
-	print(x1+x2)elif x1==0 and x2==1:
-	print(x1+x2)""" --IM_ES_N """
-elif x1==0 and x2==1:
-	print(x1+x2)elif x1==0 and x2==1:
-	print(x1+x2)elif x1==0 and x2==1:
-	print(x1+x2)elif x1==0 and x2==1:
-	print(x1+x2)elif x1==0 and x2==1:
-	print(x1+x2)elif x1==0 and x2==1:
-	print(x1+x2)""" --IM_ES_N """
-elif x1==0 and x2==1:
-	print(x1+x2)elif x1==0 and x2==1:
-	print(x1+x2)elif x1==0 and x2==1:
-	print(x1+x2)elif x1==0 and x2==1:
-	print(x1+x2)elif x1==0 and x2==1:
-	print(x1+x2)elif x1==0 and x2==1:
-	print(x1+x2)""" --IM_ES_N """
+	print(x1+x2)
+elif x1==1 and x2==2:
+	print(x1+x2)
+elif x1==2 and x2==3:
+	print(x1+x2)
+elif x1==3 and x2==4:
+	print(x1+x2)
+elif x1==4 and x2==5:
+	print(x1+x2)
+""" --IM_ES_N """
